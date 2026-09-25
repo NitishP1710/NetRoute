af7e18a9a06af4e922072282784e60db72d4bd53
Nitish Patil <162705894+NitishP1710@users.noreply.github.com>
2026-09-25 22:06:22 +0530

    Add src/router.cpp

---
+++ src/router.cpp
@@ -0,0 +1,13 @@
+#include "netroute/router.hpp"
+
+#include <utility>
+
+namespace netroute {
+
+Router::Router(std::string id, std::string ip_address)
+    : id_(std::move(id)), ip_address_(std::move(ip_address)) {}
+
+const std::string& Router::id() const noexcept { return id_; }
+const std::string& Router::ip_address() const noexcept { return ip_address_; }
+
+} // namespace netroute
